488f5630df96fa88155e0d746733350b2157ac79
user <letruongnhatphuong@gmail.com>
2025-10-23 01:18:25 -0700

    Remove day01 folder

--- day01/day1_intro.py
+++
@@ -1,7 +0,0 @@
-#Day 1: My python journey begins!
-
-name = "Phuong Le"
-age = "unknown"
-hobby = "coding and cooking"
-
-print(f"Hello, my name is {name}. I am {age} years old and I love {hobby}!")
