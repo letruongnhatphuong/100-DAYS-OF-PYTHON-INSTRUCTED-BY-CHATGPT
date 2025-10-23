d58c8470c8a4768390ae3e9ce83a38034d6be038
user <letruongnhatphuong@gmail.com>
2025-10-22 23:42:32 -0700

    Day 1: My first Python script

---
+++ day1_intro.py
@@ -0,0 +1,7 @@
+#Day 1: My python journey begins!
+
+name = "Phuong Le"
+age = "unknown"
+hobby = "coding and cooking"
+
+print(f"Hello, my name is {name}. I am {age} years old and I love {hobby}!")
